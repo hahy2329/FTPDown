75ec5de44d12f293094f614fb32b82a52a0723c3
hahy23 <gkswntjr2329@gmail.com>
2024-05-15 23:59:50 +0900

    Spring Security 정책 설정 중

--- FTPDown/src/main/java/com/application/FTP/User/service/CustomAuthenticationProvider.java
+++ FTPDown/src/main/java/com/application/FTP/User/service/CustomAuthenticationProvider.java
@@ -1,10 +1,13 @@
 package com.application.FTP.User.service;
 
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.application.FTP.User.dto.UserDTO;
 
 import lombok.RequiredArgsConstructor;
 
@@ -20,15 +23,27 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
 		UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) authentication;
 		
 		// AuthenticationFilter에서 생성된 토큰으로부터 아이디와 비밀번호를 조회함.
+		String userId = token.getName();
+		String userPw = (String)token.getCredentials();
+		
+		UserDTO uDTO = new UserDTO();
+		// DB에서 아이디로 사용자 조회
+		
+		// 비밀번호 일치 여부 체크
+		if(!bcryptPasswordEncoder.matches(userPw, uDTO.getPassword())) {
+			throw new BadCredentialsException(uDTO.getName() + " Invalid password");
+		}
+		
+		// principal(접근대상 정보), credential(비밀번호), authorities(권한 목록)를 token에 담아 반환
+		return new UsernamePasswordAuthenticationToken(uDTO, userPw, uDTO.getAuthorities());
 		
 		
-		return null;
 	}
 
 	@Override
 	public boolean supports(Class<?> authentication) {
-		// TODO Auto-generated method stub
-		return false;
+		
+		return authentication.equals(UsernamePasswordAuthenticationToken.class);
 	}
 
 }
