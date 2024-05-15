e3364160b3a064db8813e160cb020bb2a046b561
hahy23 <gkswntjr2329@gmail.com>
2024-05-16 00:10:22 +0900

    Spring Security 적용 중 : 로그인 성공과 실패시 이동 URL 설정 중

---
+++ FTPDown/src/main/java/com/application/FTP/User/service/UserCustomLoginFailHandler.java
@@ -0,0 +1,23 @@
+package com.application.FTP.User.service;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+public class UserCustomLoginFailHandler implements AuthenticationFailureHandler {
+
+	@Override
+	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+			AuthenticationException exception) throws IOException, ServletException {
+		
+		//로그인 실패 후 페이지 이동 시 해당 코드 적용
+		response.sendRedirect("/user/login");
+		
+	}
+
+}
