9d931837b09ff831df18282b342aaa75c7192337
hahy23 <gkswntjr2329@gmail.com>
2024-05-20 15:45:40 +0900

    Spring Security 정책 설정 중

--- FTPDown/src/main/java/com/application/FTP/User/service/SecurityConfig.java
+++ FTPDown/src/main/java/com/application/FTP/User/service/SecurityConfig.java
@@ -13,6 +13,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 @Configuration
 @EnableWebSecurity //SpringSecurityFilterChain 포함
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
