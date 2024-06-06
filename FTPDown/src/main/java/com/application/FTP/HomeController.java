d560ea7fb5af3b2234d7135d9db760c25a372a07
hahy23 <gkswntjr2329@gmail.com>
2024-06-06 14:13:02 +0900

    Spring Security 정책 설정 중

--- FTPDown/src/main/java/com/application/FTP/HomeController.java
+++ FTPDown/src/main/java/com/application/FTP/HomeController.java
@@ -5,14 +5,18 @@ import java.util.Date;
 import java.util.Locale;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.application.FTP.blackjack.CustomUserDetails;
 
 
 /**
@@ -27,10 +31,15 @@ public class HomeController {
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public ModelAndView home(Locale locale, Model model, HttpServletRequest request) {
+	public ModelAndView home(Locale locale, Model model, HttpServletRequest request, HttpSession session) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
 		ModelAndView mv = new ModelAndView();
+		
+		CustomUserDetails userDetails = (CustomUserDetails)SecurityContextHolder.getContext().getAuthentication().getDetails();
+		
+		logger.info("Welcome login_success! {}" + session.getId() + userDetails.getUsername() + "/" + userDetails.getPassword());
+		session.setAttribute("userLoginInfo", userDetails);
 		
 		
 		mv.setViewName("/main");
--- FTPDown/src/main/java/com/application/FTP/blackjack/CustomAuthenticationProvider.java
+++ FTPDown/src/main/java/com/application/FTP/blackjack/CustomAuthenticationProvider.java
@@ -1,11 +1,16 @@
 package com.application.FTP.blackjack;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import com.application.FTP.HomeController;
 
@@ -19,10 +24,14 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
 		String user_id = (String)authentication.getPrincipal();
 		String user_pw = (String)authentication.getCredentials();
 		
+		logger.info("Welcome authenticate! {}" + user_id + "/" + user_pw);
 		
+		List<GrantedAuthority> roles = new ArrayList<GrantedAuthority>();
 		
+		roles.add(new SimpleGrantedAuthority("ROLE_USER"));
 		
-		return null;
+		UsernamePasswordAuthenticationToken result = new UsernamePasswordAuthenticationToken(user_id, user_pw, roles);
+		return result;
 	}
 
 	@Override
